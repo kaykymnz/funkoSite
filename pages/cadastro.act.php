3e6d83eeefa00ca500957b91a6f38f89df9965f4
kaykymnz <menezeskayky16@gmail.com>
2024-10-20 13:06:40 -0300

    cadastro no banco de dados funcional

--- pages/cadastro.act.php
+++ pages/cadastro.act.php
@@ -2,29 +2,33 @@
         require('connect.php');
         @session_start();
         $cod = $_SESSION['cod'];
-        var_dump($_SESSION);
+        // var_dump($_SESSION);
 
 extract($_FILES);
 extract($_POST);
 
 
-$arquivo = "imgs/".md5(time().$foto['size']). ".jpg";
-move_uploaded_file($foto['tmp_name'],$arquivo);
+$imgFunko1 = "../imgsFunko/".md5(time().$img1['size']). ".jpg";
+move_uploaded_file($img1['tmp_name'],$imgFunko1);
 
-$arquivoCostas = "imgs/".md5(time().$fotoCostas['size']). ".jpg";
-move_uploaded_file($fotoCostas['tmp_name'],$arquivoCostas);
+$imgFunko2 = "../imgsFunko/".md5(time().$img2['size']). ".jpg";
+move_uploaded_file($img2['tmp_name'],$imgFunko2);
 
-        if(mysqli_query($con, "INSERT INTO `inf_camisetas` (`cod`,`usuario`, `dtaFab`, `dtaRecepcao`, `marca`, `modelo`, `forma`, `cor`, `edicao`, `preco`, `time`, `genero`, `liga`, `tamanho`, `foto`,`foto2`)
-         VALUES (NULL,'$cod', '$dtaFab', '$dtaRecepcao', '$marca', '$modelo', '$forma', '$cor', '$edicao', '$preco', '$time', '$genero', '$liga', '$tamanho', '$arquivo','$arquivoCostas');")){
-        $msg = "Camisa gravada com sucesso!";
+$imgFunko3 = "../imgsFunko/".md5(time().$img3['size']). ".jpg";
+move_uploaded_file($img3['tmp_name'],$imgFunko3);
+
+        if(mysqli_query($con, "INSERT INTO `produtoFunko` (`codFunko`,`codUser`,`nomeFunko`, `midiaFunko`, `catFunko`,`numFunko`, `imgFunko1`, `imgFunko2`, `imgFunko3`, `valorFunko`)
+         VALUES (NULL,'$cod', '$nomeFunko', '$midFunko', '$catFunko', '$numFunko', '$imgFunko1', '$imgFunko2', '$imgFunko3', '$precoFunko');")){
+        $msg = "Funko gravado com sucesso!";
+        echo "<a href=cadastro.php>Cadastrar Outro </a><br>";
   }else{
         $msg = "Erro ao gravar";
+        echo "<a href=cadastro.php>Tentar Novamente</a><br>";
        }
     
-
-session_start();
+echo $msg;
 $_SESSION['msg'] = $msg;
-header("location:produtos.php");
+// header("location:produtos.php");
 
 
 
--- pages/formCad.php
+++ pages/formCad.php
@@ -1,23 +1,55 @@
 <div class="divForm">
     <form class="formulario" action="cadastro.act.php" method="post" enctype="multipart/form-data">
         <p>Cadastro de Funko para venda no Site</p>
-        <input type="hidden" name="codUser" value="<?php $_SESSION['cod'] ?>">
         <p>Nome do Personagem</p>
         <input type="text" name="nomeFunko" placeholder="Exemplo: Luffy Gear Two, Western Barbie">
         <p>Categoria do Funko:</p>
-        <p><select name="catFunko" id="">
-            <option value="Serie">Série</option>
-            <option value="Animacao">Animação</option>
-            <option value="Filme">Filme</option>
-            <option value="Outro">Outro</option>
-        </select>
+        <p>
+            <select name="catFunko">
+                <option value="Serie">Série</option>
+                <option value="Animacao">Animação</option>
+                <option value="Filme">Filme</option>
+                <option value="Outro">Outro</option>
+            </select>
         </p>
-        
         <p>Nome da série/animação/filme</p>
         <input type="text" name="midFunko" placeholder="Exemplo: One Piece, Looney Tunes, Assassins Creed">
         <p>Número do funko</p>
         <p><input type="number" name="numFunko" placeholder="Localizado no canto superior direito #"></p>
+        
         <p>Imagem de capa (funko na caixa)</p>
-        <p><input type="file" name="img1"></p>
+        <input type="file" name="img1" id="img1" onchange="previewImage(event, 'preview1')">
+        <img id="preview1" style="display: none; max-width: 200px; margin-top: 10px;">
+        
+        <p>Imagem 2 (funko fora da caixa)</p>
+        <input type="file" name="img2" id="img2" onchange="previewImage(event, 'preview2')">
+        <img id="preview2" style="display: none; max-width: 200px; margin-top: 10px;">
+        
+        <p>Imagem 3</p>
+        <input type="file" name="img3" id="img3" onchange="previewImage(event, 'preview3')">
+        <img id="preview3" style="display: none; max-width: 200px; margin-top: 10px;">
+        
+        <p>Valor do Funko</p>
+        <input type="number" name="precoFunko" placeholder="Sem vírgula (Exemplo: 199)">
+        <p><input type="submit" value="Cadastrar"></p>
+    
     </form>
 </div>
+
+<script>
+    function previewImage(event, previewId) {
+        const input = event.target;
+        const preview = document.getElementById(previewId);
+        
+        if (input.files && input.files[0]) {
+            const reader = new FileReader();
+            
+            reader.onload = function(e) {
+                preview.src = e.target.result;
+                preview.style.display = 'block';
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
