dc1c1142ed428a9a7e54191042669c15305eed22
kaykymnz <menezeskayky16@gmail.com>
2024-11-01 01:35:53 -0300

    continuação da página de compra do produto

--- pages/pagCompra.php
+++ pages/pagCompra.php
@@ -7,16 +7,19 @@
     <title>Funkok POP! | Funko Log</title>
 </head>
 <body>
+    <?php include('navbar.php'); ?>
+
     <?php
-        include('navbar.php');
+        // include('navbar.php');
         require('connect.php');
 
         $codigo = $_GET['cod'];
-        // echo $codigo;
+
 
         $busca = mysqli_query($con, "Select * from `produtofunko` where `codFunko` = '$codigo'");
-        
+
         while($funko = mysqli_fetch_array($busca)){
+            $valorSemDesc = $funko['valorFunko'] + 50;
             echo "    
                 <div class=allCompra>
                     <div class=imgsCompra>
@@ -29,12 +32,25 @@
                             <img src=$funko[imgFunko1]>
                         </div>                       
                     </div>
+                    <div class=direitaCompra>
+                        <div class=titulo>Funko POP! $funko[midiaFunko] - $funko[nomeFunko] <br> #$funko[numFunko]</div>                    
+                        <div class=vendEntg> <p>VENDIDO E ENTREGUE POR: <strong>FUNKOLOG</strong> </p> <p> | </p> <p>EM ESTOQUE</p> </div>
+                        <div class=valorVenda> <p>R$$valorSemDesc,00</p> <p> R$$funko[valorFunko],00</p> </div>
+                        <div class=infsAbaixoPreco> <p>Em até <strong>12x</strong> de <strong>R$"; echo intval($funko['valorFunko'] / 12); echo ",00 </strong> sem juros no cartão Funko Log</p></div>
+                        <div class=infsAbaixoPreco2><p>Ou em <strong>1x</strong> com <strong>10% Off</strong> </p></div>
+                        <div class=opcPgmnt><p>Ver mais opções de pagamento</p></div>
+                        <div class=btnsCompraFunko> <input type=button value=Comprar> <input type=button value=\"Carrinho +\"></div>
+                    
+                    
+                    </div>
                 </div>
                 ";
             
 
         }
     ?>
+
+
 
 </body>
 </html>
